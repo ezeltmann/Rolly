d0211861d9cecaa3d8a188ce2bf1dc3c27ac3ea3
tortoise8 <leito.amanda@gmail.com>
2024-10-29 11:34:00 -0500

    Did some refactoring

--- Dice/D20.py
+++ Dice/D20.py
@@ -1,9 +1,6 @@
 from Dice.Die import Die
 
-from panda3d.bullet import BulletRigidBodyNode
-from panda3d.bullet import BulletTriangleMesh
-from panda3d.bullet import BulletTriangleMeshShape
-from panda3d.core import BitMask32
+
 from panda3d.core import Vec3
 
 
@@ -34,43 +31,14 @@ class D20(Die):
             20:Vec3(0.5773502588272095,0.5773502588272095,0.5773502588272095),
         }
 
-
     def die_setup(self, render, loader):
-        (shape, visNP) = self.setup_model(loader)
-        self._node = BulletRigidBodyNode("Die")
+        super().die_setup(render, loader)
         self._node.friction = 10.0
-        self._node.addShape(shape)
-        self._node.setMass(1000000.0)
-        self._np = render.attachNewNode(self._node)
-        self._np.setCollideMask(BitMask32.bit(0))
         self._np.setScale(1.5)
-        self._node.setDeactivationEnabled(False)
-        self._node.setCollisionResponse(True)
-        visNP.reparentTo(self._np)
-
+        self._node.setMass(1.0)
 
     def setup_model(self, loader):
-        visNP = loader.loadModel(self.file_path)
-        mesh = BulletTriangleMesh()
-
-        for node_path in visNP.find_all_matches("**/+GeomNode"):
-            for i in range(node_path.node().get_num_geoms()):
-                geom = node_path.node().get_geom(i)
-                mesh.add_geom(geom)
-
-        shape = BulletTriangleMeshShape(mesh, True)
-        visNP.clearModelNodes()
-
-        return (shape, visNP)
+        return super().setup_model(loader)
     
     def get_face_value(self, world_np):
-        faces = self.face_dict
-        high = 0
-        face = 0
-        for x, y in faces.items():
-            face_vector = world_np.get_relative_vector(self.np, y)
-            value = Vec3(0,0,1).dot(face_vector)
-            if (value > high):
-                high = value
-                face = x
-        return face
+        return super().get_face_value(world_np)
--- Dice/D6.py
+++ Dice/D6.py
@@ -1,9 +1,5 @@
 from Dice.Die import Die
 
-from panda3d.bullet import BulletRigidBodyNode
-from panda3d.bullet import BulletTriangleMesh
-from panda3d.bullet import BulletTriangleMeshShape
-from panda3d.core import BitMask32
 from panda3d.core import Vec3
 
 
@@ -20,40 +16,12 @@ class D6(Die):
                 6:Vec3(0.0,0.0,-1.0),
         }
 
-    def die_setup(self, render, loader):
-        (shape, visNP) = self.setup_model(loader)
-        self._node = BulletRigidBodyNode("Die")
-        self._node.addShape(shape)
-        self._node.setMass(1.0)
-        self._np = render.attachNewNode(self._node)
-        self._np.setCollideMask(BitMask32.bit(0))
-        self._node.setDeactivationEnabled(False)
-        self._node.setCollisionResponse(True)
-        visNP.reparentTo(self._np)
-
 
     def setup_model(self, loader):
-        visNP = loader.loadModel(self.file_path)
-        mesh = BulletTriangleMesh()
-
-        for node_path in visNP.find_all_matches("**/+GeomNode"):
-            for i in range(node_path.node().get_num_geoms()):
-                geom = node_path.node().get_geom(i)
-                mesh.add_geom(geom)
-
-        shape = BulletTriangleMeshShape(mesh, True)
-        visNP.clearModelNodes()
-
-        return (shape, visNP) 
+        return super().setup_model(loader)
+    
+    def die_setup(self, render, loader):
+        return super().die_setup(render, loader)
 
     def get_face_value(self, world_np):
-        faces = self.face_dict
-        high = 0
-        face = 0
-        for x, y in faces.items():
-            face_vector = world_np.get_relative_vector(self.np, y)
-            value = Vec3(0,0,1).dot(face_vector)
-            if (value > high):
-                high = value
-                face = x
-        return face
+        return super().get_face_value(world_np)
