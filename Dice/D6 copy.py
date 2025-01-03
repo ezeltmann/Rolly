6980f3686d7d36b6900fe7d9c79e6a17cab7bb01
ezeltmann <eric.zeltmann@gmail.com>
2025-01-03 12:21:50 -0500

    Added d4 die and initial code for it.

---
+++ Dice/D6 copy.py
@@ -0,0 +1,25 @@
+from Dice.Die import Die
+
+from panda3d.core import Vec3
+
+
+
+class D6(Die):
+    def __init__(self, path):
+        self.file_path = path
+        self.face_dict = {
+            1:Vec3(0.0,0.0,-1.0),
+            2:Vec3(0.4714045524597168,-0.8164966106414795,0.3333333134651184),
+            3:Vec3(0.4714045524597168,0.8164966106414795,0.3333333432674408),
+            4:Vec3(-0.942808985710144,0.0,0.3333333134651184),
+        }
+
+
+    def setup_model(self, loader):
+        return super().setup_model(loader)
+    
+    def die_setup(self, render, loader):
+        return super().die_setup(render, loader)
+
+    def get_face_value(self, world_np):
+        return super().get_face_value(world_np)
