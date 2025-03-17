0f0dfca5bd25fab625d64cdf822efb9f13f8a31e
ezeltmann <eric.zeltmann@gmail.com>
2025-03-17 12:17:01 -0400

    Added D3 model and basic d3 code.

---
+++ Dice/D3.py
@@ -0,0 +1,27 @@
+from Dice.Die import Die
+
+from panda3d.core import Vec3
+
+
+
+class D3(Die):
+    def __init__(self, path):
+        self.file_path = path
+        self.face_dict = {
+            1:Vec3(0.0,-0.0,1.0),
+            1.1:Vec3(0.0,1.0,0.0),
+            2:Vec3(1.0,-0.0,0.0),
+            2.1:Vec3(0.0,-1.0,0.0),
+            3:Vec3(-1.0,-0.0,0.0),
+            3.1:Vec3(0.0,0.0,-1.0)
+        }
+
+
+    def setup_model(self, loader):
+        return super().setup_model(loader)
+    
+    def die_setup(self, render, loader):
+        return super().die_setup(render, loader)
+
+    def get_face_value(self, world_np):
+        return super().get_face_value(world_np)
